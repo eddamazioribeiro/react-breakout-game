6fa66cd0626d7da40f79c84d9a4d929356e31e1a
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2021-01-13 00:31:07 -0300

    Handling arrow keys input

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Game = ({width, height, tilesize}) => {
+  useEffect(() => {
+    console.log('handle input');
+    handleInput();
+  });
+
+  const handleKeys = e => {
+    e.preventDefault();
+
+    switch (e.keyCode) {
+      // left arrow
+      case 37:
+        console.log('move left', {x: -1, y: 0});
+        break;
+      // up arrow
+      case 38:
+        console.log('move up', {x: 0, y: -1});
+        break;
+      // right arrow
+      case 39:
+        console.log('move right', {x: 1, y: 0});
+        break;
+      // down arrow
+      case 40:
+        console.log('move down', {x: 0, y: 1});
+        break;
+      default:
+        break;
+    }
+  }
+  
+  const handleInput = () => {
+    document.addEventListener('keydown', handleKeys);
+  }
   
   return(
     <>
