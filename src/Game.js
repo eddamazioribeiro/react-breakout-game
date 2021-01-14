c284a2968432453f291dcd0acc7021a4f8dfdc8e
Eduardo Ribeiro - Target <eduardo.ribeiro@targetsoftware.com.br>
2021-01-14 14:51:12 -0300

    Drawing and moving rectangle on canvas (movement not working properly yet)

--- src/Game.js
+++ src/Game.js
@@ -1,44 +1,85 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 const Game = ({width, height, tilesize}) => {
-  useEffect(() => {
-    console.log('handle input');
-    handleInput();
+  // vars
+  const gameScreen = useRef();
+  const [player, setPlayer] = useState({
+    xPos: 60,
+    yPos: 60
   });
+  
+  // hooks
+  useEffect(() => {
+    init();
+  }, []);
+
+  useEffect(() => {
+    updateGame();
+  });
+
+  // functions
+  const init = () => {
+    handleInput();
+  }
+
+  const updateGame = () => {
+    const context = gameScreen.current.getContext('2d');
+    context.clearRect(0, 0, width * tilesize, height * tilesize);
+    context.fillStyle = '';
+    context.fillRect(player.xPos, player.yPos, 16, 16);
+  }
 
   const handleKeys = e => {
     e.preventDefault();
 
+    let coord = undefined;
+
     switch (e.keyCode) {
       // left arrow
       case 37:
-        console.log('move left', {x: -1, y: 0});
+        coord = {x: -1, y: 0};
         break;
       // up arrow
       case 38:
-        console.log('move up', {x: 0, y: -1});
+        coord = {x: 0, y: -1};
         break;
       // right arrow
       case 39:
-        console.log('move right', {x: 1, y: 0});
+        coord = {x: 1, y: 0};
         break;
       // down arrow
       case 40:
-        console.log('move down', {x: 0, y: 1});
+        coord = {x: 0, y: 1};
         break;
       default:
         break;
+    }
+
+    if (coord !== undefined) {
+      let {x, y} = coord;
+  
+      movePlayer(x, y);
     }
   }
   
   const handleInput = () => {
     document.addEventListener('keydown', handleKeys);
   }
+
+  const movePlayer = (x, y) => {
+    let newPlayer = {...player};
+
+    newPlayer.xPos += x * tilesize;
+    newPlayer.yPos += y * tilesize;
+
+    setPlayer(newPlayer);
+  }
   
   return(
     <>
       <div>
         <canvas
+          ref={gameScreen}
           width={width * tilesize}
           height={height * tilesize}
           style={{
